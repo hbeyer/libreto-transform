9288b5cd77ad6a13b495c25e42beaa3127cdcb9e
Hartmut Beyer <beyer@hab.de>
2020-09-10 20:04:15 +0200

    Geodaten ergänzt

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,8 @@ class ComposerAutoloaderInit184d746a9b091c44caa840b8a05d1625
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit184d746a9b091c44caa840b8a05d1625', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        //$useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = null;
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
