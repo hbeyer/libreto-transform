457fa39a926887afcdb7659647551926f75b202d
Hartmut Beyer <hartmut_beyer@gmx.de>
2020-09-20 11:35:49 +0200

    K10plus hinzugefügt

---
+++ private/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$impressum = 'http://www.hab.de/de/home/impressum.html';
+$userGeoNames = 'hbeyer';
+$userAgentHTTP = 'Dr. Hartmut Beyer, HAB';
+
+?>
---
+++ projectFiles/sturm/index.php
@@ -0,0 +1,2 @@
+<?php 
+ header("Location: sturm-histSubject.html"); ?>
