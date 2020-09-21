48f58aadea0e09bb15729142d7b67a827696ca6a
Hartmut Beyer <hartmut_beyer@gmx.de>
2020-09-21 11:11:46 +0200

    Korrektur transform.php

--- transform.php
+++ transform.php
@@ -1,21 +1,19 @@
 <?php
 
-set_time_limit (600);
-
 require __DIR__ .'/vendor/autoload.php';
 foreach (glob("classes/class_*.php") as $filename) {
     include $filename;
 	}
 include('functions/encode.php');
 
-$reconstruction = new reconstruction('', 'sturm', 'sql_dh');
+$reconstruction = new reconstruction('{Pfad zur Datei}', '{Dateiname für das Projekt}', '{xml|csv|php|sql_dh}');
 $reconstruction->enrichData();
 $reconstruction->saveAllFormats();
 
 // Anpassen zum Eingrenzen der darzustellenden Felder
-$pages = array('histSubject', 'persName', 'year', 'subjects', 'genres', 'languages', 'placeName', 'format', 'systemManifestation');
-$doughnuts = array('format', 'histSubject', 'subjects', 'genres', 'languages', 'systemManifestation', 'beacon');
-$clouds = array('subjects', 'genres', 'persName', 'placeName');
+$pages = array('numberCat', 'catSubjectFormat', 'shelfmarkOriginal', 'histSubject', 'persName', 'gender', 'beacon', 'year', 'subjects', 'histShelfmark', 'genres', 'languages', 'placeName', 'publishers', 'format', 'volumes', 'mediaType', 'bound', 'systemManifestation', 'institutionOriginal', 'provenanceAttribute', 'pageCat', 'titleWork', 'borrower', 'dateLending');
+$doughnuts = array('persName', 'gender', 'format', 'histSubject', 'subjects', 'genres', 'mediaType', 'languages', 'systemManifestation', 'institutionOriginal', 'provenanceAttribute', 'bound', 'beacon');
+$clouds = array('publishers', 'format', 'histSubject', 'subjects', 'genres', 'mediaType', 'persName', 'gnd', 'role', 'placeName', 'languages', 'systemManifestation', 'institutionOriginal', 'shelfmarkOriginal', 'provenanceAttribute', 'beacon', 'borrower');
 
 $facetList = new facetList($pages, $doughnuts, $clouds);
 $frontend = new frontend($reconstruction, $facetList);
